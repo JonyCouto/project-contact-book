0943e9fb42c6734b09eda3486596de083e58b7af
Jonathan <jonathancouto3030@hotmail.com>
2024-05-08 17:42:57 -0300

    Início do projeto

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+// vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+const vuetify = createVuetify({ components, directives })
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router).use(vuetify)
+
+app.mount('#app')
